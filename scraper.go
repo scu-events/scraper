2e0637b5eb8e60b74965cbb454dde6204c708b9e
MattMistele <matthew@mistele.com>
2018-03-10 15:17:42 -0800

    Added simple webscraper for google calenders

---
+++ scraper.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type DateTime struct {
+	DateTime string `json:"datetime"`
+	TimeZone string `json:"timeZone"`
+}
+
+type Event struct {
+	Summary     string   `json:"summary"`
+	Description string   `json:"description"`
+	Location    string   `json:"location"`
+	Start       DateTime `json:"start"`
+	end         DateTime `json:"end"`
+}
+
+func Crawl(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var data map[string][]Event
+	json.Unmarshal(body, &data)
+
+	fmt.Printf("\nData \n")
+	for _, item := range data["items"] {
+		fmt.Printf("Summary:\n")
+		fmt.Println(item.Summary)
+		fmt.Printf("Description:\n")
+		fmt.Println(item.Description)
+	}
+	return
+}
+
+func main() {
+	urls := []string{"https://www.googleapis.com/calendar/v3/calendars/santaclara.acm@gmail.com/events?key=AIzaSyCnRyFyPuJ9WSeu602Q7CE13TsxWVNbw10&timeMin=2018-02-24T00:00:00Z&timeMax=2018-04-09T00:00:00Z&singmaxResults=9999&_=1520708172234", "https://clients6.google.com/calendar/v3/calendars/csl@scu.edu/events?calendarId=csl@scu.edu&singleEvents=true&timeZone=America/Los_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-02-26T00:00:00-08:00&timeMax=2018-04-02T00:00:00-08:00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"}
+	for _, url := range urls {
+		Crawl(url)
+	}
+}
