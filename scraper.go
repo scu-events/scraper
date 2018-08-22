e498776c1b0f8c22d3434057a20e75c8e4190848
Kade <kade_harmon@yahoo.com>
2018-08-22 04:36:56 -0700

    Update
    Cleared warnings, added SCU official calendar to private google calendar, changed calendar we were hosting data on

--- scraper.go
+++ scraper.go
@@ -3,26 +3,26 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 	"io/ioutil"
+	"time"
 	//"log"
 	"net/http"
 	"strings"
 
-	"github.com/mmcdole/gofeed"
-
-//	"golang.org/x/net/context"
-//  "golang.org/x/oauth2"
- // "golang.org/x/oauth2/google"
-    "google.golang.org/api/calendar/v3"
+	//	"golang.org/x/net/context"
+	//  "golang.org/x/oauth2"
+	// "golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
 )
 
+//DateTime struct
 type DateTime struct {
 	DateTime string `json:"datetime"`
 	TimeZone string `json:"timeZone"`
 }
 
-func CrawlGoogleCal(url string, calendarId string) {
+//CrawlGoogleCal gathers data from google calendars
+func CrawlGoogleCal(url string, calendarID string) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
@@ -38,15 +38,11 @@ func CrawlGoogleCal(url string, calendarId string) {
 	var data map[string][]calendar.Event
 	json.Unmarshal(body, &data)
 
-	// Throw event into database
-
-
 	// printing out info
 	fmt.Printf("\n O \n")
 	for _, item := range data["items"] {
-		item.Summary = strings.TrimSpace(item.Summary);
-		item.Description = strings.TrimSpace(item.Description);	
-
+		item.Summary = strings.TrimSpace(item.Summary)
+		item.Description = strings.TrimSpace(item.Description)
 
 		fmt.Println(item.Summary)
 		fmt.Printf("Start Time: ")
@@ -56,56 +52,38 @@ func CrawlGoogleCal(url string, calendarId string) {
 		fmt.Printf("id:  ")
 		fmt.Println(item.Id)
 		fmt.Printf("time:  ")
-		fmt.Println(item.Id)
+		fmt.Println(item.Id) //?
 
 		fmt.Printf("\n")
 
-	//	event, err = srv.Events.Insert(calendarId, item).Do()
-	//	if err != nil {
-  	//		log.Fatalf("Unable to create event. %v\n", err)
-	//	}
-		
-	//	fmt.Printf("Event created: %s\n", item.HtmlLink)
+		//	event, err = srv.Events.Insert(calendarId, item).Do()
+		//	if err != nil {
+		//		log.Fatalf("Unable to create event. %v\n", err)
+		//	}
+
+		//	fmt.Printf("Event created: %s\n", item.HtmlLink)
 
 	}
-	return
-}
-
-func CrawlOfficialCal(url string) {
-
-	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL(url)
-	fmt.Println(feed.Title)
-	fmt.Println()
-
-	for _, element := range feed.Items {
-		fmt.Println(element.Title)
-
-		fmt.Printf("	")
-		fmt.Printf(element.Description)
-	}
-
 	return
 }
 
 func main() {
 
-	calendarId := "info.scuevents@gmail.com"
+	//Host Calendar
+	calendarID := "scuhackers@gmail.com"
 
-	//GOOGLE CAL URLS	
-	//acm club: http://acm.engr.scu.edu/events
-	var url_google_acm = "https://www.googleapis.com/calendar/v3/calendars/santaclara.acm@gmail.com/events?key=AIzaSyCnRyFyPuJ9WSeu602Q7CE13TsxWVNbw10&timeMin=2018-02-24T00:00:00Z&timeMax=2030-04-09T00:00:00Z&singmaxResults=9999&_=1520708172234"
-	
-	//offical calendar of CSO's and RSO's: https://www.scu.edu/csi/calendar/
-//	var url_google_clubs = "https://clients6.google.com/calendar/v3/calendars/csl@scu.edu/events?calendarId=csl@scu.edu&singleEvents=true&timeZone=America/Los_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-02-26T00:00:00-08:00&timeMax=2018-04-02T00:00:00-08:00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"
+	//Array of Google Calendars to Scrape
+	var googleCals [2]string
 
+	//ACM Club Calendar
+	googleCals[0] = "https://www.googleapis.com/calendar/v3/calendars/santaclara.acm@gmail.com/events?key=AIzaSyCnRyFyPuJ9WSeu602Q7CE13TsxWVNbw10&timeMin=2018-02-24T00:00:00Z&timeMax=2030-04-09T00:00:00Z&singmaxResults=9999&_=1520708172234"
+	//Official CSO/RSO Calendar
+	googleCals[1] = "https://clients6.google.com/calendar/v3/calendars/csl@scu.edu/events?calendarId=csl@scu.edu&singleEvents=true&timeZone=America/Los_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-02-26T00:00:00-08:00&timeMax=2018-04-02T00:00:00-08:00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"
 
-//	var officialUrl = "https://lwcal.scu.edu/live/rss/events/exclude_group/Math%20Tutors/exclude_tag/private/header/All%20Events"
-
-	//CrawlGoogleCal(url_google_clubs)
-	CrawlGoogleCal(url_google_acm, calendarId)
-
-//	CrawlOfficialCal(officialUrl)
+	//Crawl through Google Calendars
+	for _, item := range googleCals {
+		CrawlGoogleCal(item, calendarID)
+	}
 
 	t := time.Now()
 	fmt.Println(t.Format(time.RFC850))
@@ -113,11 +91,11 @@ func main() {
 }
 
 //Using natural language processing to determine if an event has free food
-func freeFood(description string) bool{
+func freeFood(description string) bool {
 	foodWords := [7]string{"food", "refreshments", "dinner", "lunch", "breakfast", "snacks", "drinks"}
 
 	for _, element := range foodWords {
-		if(strings.Contains(description, element)){
+		if strings.Contains(description, element) {
 			return true
 		}
 	}
